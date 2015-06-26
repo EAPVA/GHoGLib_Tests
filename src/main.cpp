278ec485b0dd9d7df2b25ee286ce8c59a08b442e
Teider <teiderlopes@gmail.com>
2015-06-26 18:23:35 -0300

    First consistent version.

--- src/main.cpp
+++ src/main.cpp
@@ -14,6 +14,8 @@
 #include <include/HogDescriptor.h>
 #include <include/HogGPU.h>
 
+#include <cmath>
+
 #include "utils.h"
 
 std::vector< double > calculate_statistics(std::vector< double > values)
@@ -78,17 +80,20 @@ void measure_time(ghog::lib::HogDescriptor* hog,
 		<< num_experiments << " windows of size " << window_size << std::endl;
 
 	cv::Mat input_img;
+	cv::Mat normalized_img;
 	cv::Mat grad_mag;
 	cv::Mat grad_phase;
 	cv::Mat descriptor;
 	cv::Size descriptor_size(hog->get_descriptor_size(), 1);
 
 	hog->alloc_buffer(img_size, CV_32FC3, input_img);
+	hog->alloc_buffer(window_size, CV_32FC3, normalized_img);
 	hog->alloc_buffer(window_size, CV_32FC1, grad_mag);
 	hog->alloc_buffer(window_size, CV_32FC1, grad_phase);
 	hog->alloc_buffer(descriptor_size, CV_32FC1, descriptor);
 
 	input_img.addref();
+	normalized_img.addref();
 	grad_mag.addref();
 	grad_phase.addref();
 	descriptor.addref();
@@ -121,8 +126,8 @@ void measure_time(ghog::lib::HogDescriptor* hog,
 
 		hog->image_normalization_sync(input_img);
 		hog->calc_gradient_sync(
-			input_img.rowRange(pos_y, pos_y + window_size.height + 1).colRange(
-				pos_x, pos_x + window_size.width + 1), grad_mag, grad_phase);
+			input_img.rowRange(pos_y, pos_y + window_size.height).colRange(
+				pos_x, pos_x + window_size.width), grad_mag, grad_phase);
 		hog->create_descriptor_sync(grad_mag, grad_phase, descriptor);
 	}
 
@@ -137,16 +142,20 @@ void measure_time(ghog::lib::HogDescriptor* hog,
 			int pos_x = dist_w(random_gen);
 			int pos_y = dist_h(random_gen);
 
+			input_img.rowRange(pos_y, pos_y + window_size.height).colRange(
+				pos_x, pos_x + window_size.width).copyTo(normalized_img);
+
 			start = boost::chrono::steady_clock::now();
-			hog->image_normalization_sync(input_img);
+			hog->image_normalization_sync(normalized_img);
 			time_elapsed = boost::chrono::steady_clock::now() - start;
 			time_elapsed_normalization = time_elapsed.count();
-			hog->calc_gradient_sync(
-				input_img.rowRange(pos_y, pos_y + window_size.height + 1)
-					.colRange(pos_x, pos_x + window_size.width + 1), grad_mag,
-				grad_phase);
+
+			start = boost::chrono::steady_clock::now();
+			hog->calc_gradient_sync(normalized_img, grad_mag, grad_phase);
 			time_elapsed = boost::chrono::steady_clock::now() - start;
 			time_elapsed_gradient = time_elapsed.count();
+
+			start = boost::chrono::steady_clock::now();
 			hog->create_descriptor_sync(grad_mag, grad_phase, descriptor);
 			time_elapsed = boost::chrono::steady_clock::now() - start;
 			time_elapsed_descriptor = time_elapsed.count();
@@ -171,42 +180,27 @@ void measure_time(ghog::lib::HogDescriptor* hog,
 	std::cout << std::endl;
 }
 
-int count = 0;
-
 double compare_matrices(cv::Mat m1,
 	cv::Mat m2)
 {
-	double dot_product = 0.0;
-	double mod_1 = 0.0;
-	double mod_2 = 0.0;
-//	double sum_squares_ref = 0.0;
-//	double sum_squares_diff = 0.0;
-//	cv::Mat diff = m1 - m2;
+	double distance = 0.0;
+	double magnitude_1 = 0.0;
+	double magnitude_2 = 0.0;
+	int num_different = 0;
 	for(int i = 0; i < m1.rows; ++i)
 	{
 		for(int j = 0; j < m1.cols; ++j)
 		{
 			double m1_ij = (double)m1.at< float >(i, j);
-			double m2_ij = (double)m1.at< float >(i, j);
-			mod_1 += m1_ij * m1_ij;
-			mod_2 += m2_ij * m2_ij;
-			dot_product += m1_ij * m2_ij;
-//			double diff_ij = (double)diff.at< float >(i, j);
-//			sum_squares_ref += m1_ij * m1_ij;
-//			sum_squares_diff += diff_ij * diff_ij;
+			double m2_ij = (double)m2.at< float >(i, j);
+			magnitude_1 += (m1_ij * m1_ij);
+			magnitude_2 += (m2_ij * m2_ij);
+			double difference = abs(m1_ij - m2_ij);
+			distance += difference * difference;
 		}
 	}
-	if(count == 50)
-	{
-		std::cout << count << ": " << "Mod 1: " << mod_1 << "  Mod 2: " << mod_2
-			<< "  dot: " << dot_product << std::endl;
-	}
-	count++;
-	mod_1 = sqrt(mod_1);
-	mod_2 = sqrt(mod_2);
-	return dot_product / (mod_1 * mod_2);
-//	sum_squares_diff /= sum_squares_ref;
-//	return 100 * sqrt(sum_squares_diff);
+	double sum_magnitudes = sqrt(magnitude_1) + sqrt(magnitude_2);
+	return sqrt(distance) / sum_magnitudes;
 }
 
 void measure_difference(ghog::lib::HogDescriptor* hog1,
@@ -220,38 +214,39 @@ void measure_difference(ghog::lib::HogDescriptor* hog1,
 	boost::random::mt19937 random_gen)
 {
 	std::cout << "Running difference experiment on descriptors " << hog_name1
-		<< " and " << hog_name2 << ", with " << image_list.size()
+
+	<< " and " << hog_name2 << ", with " << image_list.size()
 		<< " images, using " << num_experiments << " windows of size "
 		<< window_size << std::endl;
 
 	cv::Size descriptor_size(hog1->get_descriptor_size(), 1);
 
 	cv::Mat input_img1;
+	cv::Mat normalized_img1;
 	cv::Mat grad_mag1;
 	cv::Mat grad_phase1;
 	cv::Mat descriptor1;
 
 	hog1->alloc_buffer(img_size, CV_32FC3, input_img1);
+	hog1->alloc_buffer(window_size, CV_32FC3, normalized_img1);
 	hog1->alloc_buffer(window_size, CV_32FC1, grad_mag1);
 	hog1->alloc_buffer(window_size, CV_32FC1, grad_phase1);
 	hog1->alloc_buffer(descriptor_size, CV_32FC1, descriptor1);
 
-	input_img1.addref();
-	grad_mag1.addref();
-	grad_phase1.addref();
-	descriptor1.addref();
-
 	cv::Mat input_img2;
+	cv::Mat normalized_img2;
 	cv::Mat grad_mag2;
 	cv::Mat grad_phase2;
 	cv::Mat descriptor2;
 
 	hog2->alloc_buffer(img_size, CV_32FC3, input_img2);
+	hog2->alloc_buffer(window_size, CV_32FC3, normalized_img2);
 	hog2->alloc_buffer(window_size, CV_32FC1, grad_mag2);
 	hog2->alloc_buffer(window_size, CV_32FC1, grad_phase2);
 	hog2->alloc_buffer(descriptor_size, CV_32FC1, descriptor2);
 
 	input_img2.addref();
+	normalized_img2.addref();
 	grad_mag2.addref();
 	grad_phase2.addref();
 	descriptor2.addref();
@@ -261,7 +256,7 @@ void measure_difference(ghog::lib::HogDescriptor* hog1,
 	boost::random::uniform_smallint< int > dist_h(1,
 		input_img1.rows - window_size.height - 2);
 
-//	std::vector< double > errors_normalization;
+	std::vector< double > errors_normalization;
 	std::vector< double > magnitude_similarity;
 	std::vector< double > phase_similarity;
 	std::vector< double > descriptor_similarity;
@@ -283,17 +278,20 @@ void measure_difference(ghog::lib::HogDescriptor* hog1,
 			int pos_x = dist_w(random_gen);
 			int pos_y = dist_h(random_gen);
 
-//			hog1->image_normalization_sync(input_img1);
-//			hog2->image_normalization_sync(input_img2);
-//			errors_normalization.push_back(
-//				compare_matrices(input_img1, input_img2));
+			input_img1.rowRange(pos_y, pos_y + window_size.height).colRange(
+				pos_x, pos_x + window_size.width).copyTo(normalized_img1);
+			input_img2.rowRange(pos_y, pos_y + window_size.height).colRange(
+				pos_x, pos_x + window_size.width).copyTo(normalized_img2);
 
-			hog1->calc_gradient_sync(
-				input_img1.rowRange(pos_y, pos_y + window_size.height).colRange(
-					pos_x, pos_x + window_size.width), grad_mag1, grad_phase1);
-			hog2->calc_gradient_sync(
-				input_img2.rowRange(pos_y, pos_y + window_size.height).colRange(
-					pos_x, pos_x + window_size.width), grad_mag2, grad_phase2);
+			hog1->image_normalization_sync(normalized_img1);
+			hog2->image_normalization_sync(normalized_img2);
+			errors_normalization.push_back(
+				compare_matrices(normalized_img1, normalized_img2));
+
+			normalized_img1.copyTo(normalized_img2);
+
+			hog1->calc_gradient_sync(normalized_img1, grad_mag1, grad_phase1);
+			hog2->calc_gradient_sync(normalized_img2, grad_mag2, grad_phase2);
 			magnitude_similarity.push_back(
 				compare_matrices(grad_mag1, grad_mag2));
 			phase_similarity.push_back(
@@ -305,91 +303,20 @@ void measure_difference(ghog::lib::HogDescriptor* hog1,
 			hog1->create_descriptor_sync(grad_mag1, grad_phase1, descriptor1);
 			hog2->create_descriptor_sync(grad_mag2, grad_phase2, descriptor2);
 
-			if(count == 50)
-			{
-//				cv::normalize(grad_mag1, grad_mag1, 0.0, 1.0, cv::NORM_MINMAX,
-//					CV_32FC1);
-//				cv::normalize(grad_mag2, grad_mag2, 0.0, 1.0, cv::NORM_MINMAX,
-//					CV_32FC1);
-//				cv::normalize(grad_phase1, grad_phase1, 0.0, 1.0,
-//					cv::NORM_MINMAX, CV_32FC1);
-//				cv::normalize(grad_phase2, grad_phase2, 0.0, 1.0,
-//					cv::NORM_MINMAX, CV_32FC1);
-//
-//				cv::imshow("i1",
-//					input_img1.rowRange(pos_y, pos_y + window_size.height)
-//						.colRange(pos_x, pos_x + window_size.width));
-//				cv::imshow("i2",
-//					input_img2.rowRange(pos_y, pos_y + window_size.height)
-//						.colRange(pos_x, pos_x + window_size.width));
-//				cv::imshow("m1", grad_mag1);
-//				cv::imshow("m2", grad_mag2);
-//				cv::imshow("p1", grad_phase1);
-//				cv::imshow("p2", grad_phase2);
-
-				cv::Mat input_1_tmp = input_img1.rowRange(pos_y,
-					pos_y + window_size.height).colRange(pos_x,
-					pos_x + window_size.width);
-
-				cv::Mat input_2_tmp = input_img2.rowRange(pos_y,
-					pos_y + window_size.height).colRange(pos_x,
-					pos_x + window_size.width);
-
-				for(int i = 0; i < input_1_tmp.rows; ++i)
-				{
-					for(int j = 0; j < input_1_tmp.cols; ++j)
-					{
-						if((i >= 80) && (i <= 87) && (j >= 40) && (j <= 47))
-						{
-							std::cout << i << " " << j << "->  Image 1: "
-								<< input_1_tmp.at< float >(i, j) << "  Mag 1: "
-								<< grad_mag1.at< float >(i, j) << "  Phase 1: "
-								<< grad_phase1.at< float >(i, j) << std::endl;
-							std::cout << i << " " << j << "->  Image 2: "
-								<< input_2_tmp.at< float >(i, j) << "  Mag 2: "
-								<< grad_mag2.at< float >(i, j) << "  Phase 2: "
-								<< grad_phase2.at< float >(i, j) << std::endl;
-						}
-					}
-				}
-
-				std::cout << std::endl << std::endl;
-			for(int i = 0; i < descriptor1.cols; ++i)
-			{
-				std::cout << i << "-> desc1: " << descriptor1.at< float >(i)
-					<< "  desc2: " << descriptor2.at< float >(i) << std::endl;
-			}
-			std::cout << std::endl << std::endl;
-
-//				cv::waitKey();
-			}
-
 			descriptor_similarity.push_back(
 				compare_matrices(descriptor1, descriptor2));
 		}
 	}
 
-//	std::cout << "Error on image normalization:" << std::endl;
-//	report_statistics(errors_normalization, 1, "\%");
+	std::cout << "Error on image normalization:" << std::endl;
+	report_statistics(errors_normalization, 1, "normalized euclidian distance");
 	std::cout << "Error on magnitude calculation:" << std::endl;
-	report_statistics(magnitude_similarity, 1, "similarity");
+	report_statistics(magnitude_similarity, 1, "normalized euclidian distance");
 	std::cout << "Error on phase calculation:" << std::endl;
-	report_statistics(phase_similarity, 1, "similarity");
+	report_statistics(phase_similarity, 1, "normalized euclidian distance");
 	std::cout << "Error on descriptor calculation:" << std::endl;
-	report_statistics(descriptor_similarity, 1, "similarity");
-
-//	std::cout << "Magnitude similarity:" << std::endl;
-//	for(int i = 0; i < magnitude_similarity.size(); ++i)
-//	{
-//		std::cout << magnitude_similarity[i] << "  ";
-//	}
-//	std::cout << std::endl;
-//	std::cout << "Phase similarity:" << std::endl;
-//	for(int i = 0; i < phase_similarity.size(); ++i)
-//	{
-//		std::cout << phase_similarity[i] << "  ";
-//	}
-//	std::cout << std::endl;
+	report_statistics(descriptor_similarity, 1,
+		"normalized euclidian distance");
 
 	std::cout << "Calculating complete difference" << std::endl;
 
@@ -407,16 +334,17 @@ void measure_difference(ghog::lib::HogDescriptor* hog1,
 			int pos_x = dist_w(random_gen);
 			int pos_y = dist_h(random_gen);
 
-//			hog1->image_normalization_sync(input_img1);
-			hog1->calc_gradient_sync(
-				input_img1.rowRange(pos_y, pos_y + window_size.height).colRange(
-					pos_x, pos_x + window_size.width), grad_mag1, grad_phase1);
+			input_img1.rowRange(pos_y, pos_y + window_size.height).colRange(
+				pos_x, pos_x + window_size.width).copyTo(normalized_img1);
+			input_img2.rowRange(pos_y, pos_y + window_size.height).colRange(
+				pos_x, pos_x + window_size.width).copyTo(normalized_img2);
+
+			hog1->image_normalization_sync(input_img1);
+			hog1->calc_gradient_sync(normalized_img1, grad_mag1, grad_phase1);
 			hog1->create_descriptor_sync(grad_mag1, grad_phase1, descriptor1);
 
-//			hog2->image_normalization_sync(input_img2);
-			hog2->calc_gradient_sync(
-				input_img2.rowRange(pos_y, pos_y + window_size.height).colRange(
-					pos_x, pos_x + window_size.width), grad_mag2, grad_phase2);
+			hog2->image_normalization_sync(input_img2);
+			hog2->calc_gradient_sync(normalized_img2, grad_mag2, grad_phase2);
 			hog2->create_descriptor_sync(grad_mag2, grad_phase2, descriptor2);
 
 			total_similarity.push_back(
@@ -424,22 +352,8 @@ void measure_difference(ghog::lib::HogDescriptor* hog1,
 		}
 	}
 
-	std::cout << "Total error :" << std::endl;
-	report_statistics(total_similarity, 1, "similarity");
-	std::cout << std::endl;
-
-	std::cout << "Descriptor similarity:" << std::endl;
-	for(int i = 0; i < descriptor_similarity.size(); ++i)
-	{
-		std::cout << descriptor_similarity[i] << "  ";
-	}
-	std::cout << std::endl;
-
-	std::cout << "Total similarity:" << std::endl;
-	for(int i = 0; i < total_similarity.size(); ++i)
-	{
-		std::cout << total_similarity[i] << "  ";
-	}
+	std::cout << "Total similarity :" << std::endl;
+	report_statistics(total_similarity, 1, "normalized euclidian distance");
 	std::cout << std::endl;
 }
 
@@ -456,10 +370,10 @@ int main(int argc,
 
 	boost::random::mt19937 random_gen;
 
-//	measure_time(hog_cpu, "Hog_CPU", file_list, img_size, window_size,
-//		num_experiments, random_gen);
-//	measure_time(hog_gpu, "Hog_GPU", file_list, img_size, window_size,
-//		num_experiments, random_gen);
+	measure_time(hog_cpu, "Hog_CPU", file_list, img_size, window_size,
+		num_experiments, random_gen);
+	measure_time(hog_gpu, "Hog_GPU", file_list, img_size, window_size,
+		num_experiments, random_gen);
 	measure_difference(hog_cpu, hog_gpu, "Hog CPU", "Hog GPU", file_list,
 		img_size, window_size, num_experiments, random_gen);
 
