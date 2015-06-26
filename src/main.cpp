d18f4cff52a5d5125045be72dcb3f5099e7aec1b
Teider <teiderlopes@gmail.com>
2015-06-26 19:04:04 -0300

    Hide partial result difference.

--- src/main.cpp
+++ src/main.cpp
@@ -262,61 +262,61 @@ void measure_difference(ghog::lib::HogDescriptor* hog1,
 	std::vector< double > descriptor_similarity;
 	std::vector< double > total_similarity;
 
-	std::cout << "Calculating partial difference" << std::endl;
-
-	for(int i = 0; i < image_list.size(); ++i)
-	{
-		cv::imread(image_list[i], CV_LOAD_IMAGE_COLOR).convertTo(input_img1,
-			CV_32FC3);
-		cv::imread(image_list[i], CV_LOAD_IMAGE_COLOR).convertTo(input_img2,
-			CV_32FC3);
-		input_img1 /= 256.0;
-		input_img2 /= 256.0;
-
-		for(int j = 0; j < num_experiments; ++j)
-		{
-			int pos_x = dist_w(random_gen);
-			int pos_y = dist_h(random_gen);
-
-			input_img1.rowRange(pos_y, pos_y + window_size.height).colRange(
-				pos_x, pos_x + window_size.width).copyTo(normalized_img1);
-			input_img2.rowRange(pos_y, pos_y + window_size.height).colRange(
-				pos_x, pos_x + window_size.width).copyTo(normalized_img2);
-
-			hog1->image_normalization_sync(normalized_img1);
-			hog2->image_normalization_sync(normalized_img2);
-			errors_normalization.push_back(
-				compare_matrices(normalized_img1, normalized_img2));
-
-			normalized_img1.copyTo(normalized_img2);
-
-			hog1->calc_gradient_sync(normalized_img1, grad_mag1, grad_phase1);
-			hog2->calc_gradient_sync(normalized_img2, grad_mag2, grad_phase2);
-			magnitude_similarity.push_back(
-				compare_matrices(grad_mag1, grad_mag2));
-			phase_similarity.push_back(
-				compare_matrices(grad_phase1, grad_phase2));
-
-			grad_mag1.copyTo(grad_mag2);
-			grad_phase1.copyTo(grad_phase2);
-
-			hog1->create_descriptor_sync(grad_mag1, grad_phase1, descriptor1);
-			hog2->create_descriptor_sync(grad_mag2, grad_phase2, descriptor2);
-
-			descriptor_similarity.push_back(
-				compare_matrices(descriptor1, descriptor2));
-		}
-	}
-
-	std::cout << "Error on image normalization:" << std::endl;
-	report_statistics(errors_normalization, 1, "normalized euclidian distance");
-	std::cout << "Error on magnitude calculation:" << std::endl;
-	report_statistics(magnitude_similarity, 1, "normalized euclidian distance");
-	std::cout << "Error on phase calculation:" << std::endl;
-	report_statistics(phase_similarity, 1, "normalized euclidian distance");
-	std::cout << "Error on descriptor calculation:" << std::endl;
-	report_statistics(descriptor_similarity, 1,
-		"normalized euclidian distance");
+//	std::cout << "Calculating partial difference" << std::endl;
+//
+//	for(int i = 0; i < image_list.size(); ++i)
+//	{
+//		cv::imread(image_list[i], CV_LOAD_IMAGE_COLOR).convertTo(input_img1,
+//			CV_32FC3);
+//		cv::imread(image_list[i], CV_LOAD_IMAGE_COLOR).convertTo(input_img2,
+//			CV_32FC3);
+//		input_img1 /= 256.0;
+//		input_img2 /= 256.0;
+//
+//		for(int j = 0; j < num_experiments; ++j)
+//		{
+//			int pos_x = dist_w(random_gen);
+//			int pos_y = dist_h(random_gen);
+//
+//			input_img1.rowRange(pos_y, pos_y + window_size.height).colRange(
+//				pos_x, pos_x + window_size.width).copyTo(normalized_img1);
+//			input_img2.rowRange(pos_y, pos_y + window_size.height).colRange(
+//				pos_x, pos_x + window_size.width).copyTo(normalized_img2);
+//
+//			hog1->image_normalization_sync(normalized_img1);
+//			hog2->image_normalization_sync(normalized_img2);
+//			errors_normalization.push_back(
+//				compare_matrices(normalized_img1, normalized_img2));
+//
+//			normalized_img1.copyTo(normalized_img2);
+//
+//			hog1->calc_gradient_sync(normalized_img1, grad_mag1, grad_phase1);
+//			hog2->calc_gradient_sync(normalized_img2, grad_mag2, grad_phase2);
+//			magnitude_similarity.push_back(
+//				compare_matrices(grad_mag1, grad_mag2));
+//			phase_similarity.push_back(
+//				compare_matrices(grad_phase1, grad_phase2));
+//
+//			grad_mag1.copyTo(grad_mag2);
+//			grad_phase1.copyTo(grad_phase2);
+//
+//			hog1->create_descriptor_sync(grad_mag1, grad_phase1, descriptor1);
+//			hog2->create_descriptor_sync(grad_mag2, grad_phase2, descriptor2);
+//
+//			descriptor_similarity.push_back(
+//				compare_matrices(descriptor1, descriptor2));
+//		}
+//	}
+//
+//	std::cout << "Error on image normalization:" << std::endl;
+//	report_statistics(errors_normalization, 1, "normalized euclidian distance");
+//	std::cout << "Error on magnitude calculation:" << std::endl;
+//	report_statistics(magnitude_similarity, 1, "normalized euclidian distance");
+//	std::cout << "Error on phase calculation:" << std::endl;
+//	report_statistics(phase_similarity, 1, "normalized euclidian distance");
+//	std::cout << "Error on descriptor calculation:" << std::endl;
+//	report_statistics(descriptor_similarity, 1,
+//		"normalized euclidian distance");
 
 	std::cout << "Calculating complete difference" << std::endl;
 
@@ -366,7 +366,7 @@ int main(int argc,
 	ghog::lib::HogDescriptor* hog_gpu = new ghog::lib::HogGPU("hog.xml");
 	cv::Size img_size(3648, 2736);
 	cv::Size window_size(64, 128);
-	int num_experiments = 10;
+	int num_experiments = 1000;
 
 	boost::random::mt19937 random_gen;
 
